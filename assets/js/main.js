9520d66cabe279c3010fd12b5ec770559b1f01ee
Joyce Chau <jyc.chau@gmail.com>
2019-05-26 23:27:32 -0700

    Fix banner flickering bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,8 +24,6 @@
 			$banner = $('#banner');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
